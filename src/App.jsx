2c2f211a47b2a886b762b9f322698d3716e39db6
sofiagakalo1 <sofiagakalo1@gmail.com>
2023-01-15 22:48:52 +0100

    task1

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import css from 'App.module.css';
+import {Profile} from './components/Profile/Profile';
+import user from './data/user.json';
+
+export const App = () => {
+  return (
+    <>
+    <div className={css.wrap}>
+    <Profile
+    username={user.username}
+    tag={user.tag}
+    location={user.location}
+    avatar={user.avatar}
+    stats={user.stats}
+  />
+    </div>
+    </>
+  );
+};
